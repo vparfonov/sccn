c7150b14806fa9669be5e7090237571934bb0aee
Tyler Holmgren <tyler@hercules.tjh.pw>
2014-10-23 23:58:04 -0500

    Changed links to feeds

--- gen/pw/tjh/android/SCCNotifications/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package pw.tjh.android.SCCNotifications;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
--- gen/pw/tjh/android/SCCNotifications/R.java
+++ gen/pw/tjh/android/SCCNotifications/R.java
@@ -19,28 +19,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button1=0x7f060003;
-        public static final int Button2=0x7f060004;
-        public static final int Button3=0x7f060005;
-        public static final int Button4=0x7f060006;
-        public static final int Button5=0x7f060007;
-        public static final int Button6=0x7f060008;
-        public static final int RssDate=0x7f06000a;
-        public static final int RssDetail=0x7f06000b;
-        public static final int RssTitle=0x7f060009;
-        public static final int detailsTextView=0x7f060000;
-        public static final int detailsWebView=0x7f060001;
-        public static final int listMainView=0x7f060002;
+        public static final int Button1=0x7f060000;
+        public static final int Button2=0x7f060001;
+        public static final int Button3=0x7f060002;
+        public static final int Button4=0x7f060003;
+        public static final int Button5=0x7f060004;
+        public static final int Button6=0x7f060005;
+        public static final int RssDate=0x7f060007;
+        public static final int RssDetail=0x7f060008;
+        public static final int RssTitle=0x7f060006;
+        public static final int listMainView=0x7f060009;
     }
     public static final class layout {
-        public static final int atomdetail=0x7f030000;
-        public static final int atomfeed=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int rssdetail=0x7f030003;
-        public static final int rssfeed=0x7f030004;
+        public static final int main=0x7f030000;
+        public static final int rssdetail=0x7f030001;
+        public static final int rssfeed=0x7f030002;
     }
     public static final class string {
-        public static final int AtomTitle=0x7f050010;
         public static final int Button1=0x7f050001;
         public static final int Button2=0x7f050002;
         public static final int Button3=0x7f050003;
@@ -48,16 +43,16 @@ public final class R {
         public static final int Button5=0x7f050005;
         public static final int Button6=0x7f050006;
         public static final int Link1=0x7f050007;
-        /**  North Mankato Events ATOM 
+        /**  North Mankato Events RSS 
          */
         public static final int Link2=0x7f050008;
-        /**  Faribault Events ATOM 
+        /**  Faribault Events RSS 
          */
         public static final int Link3=0x7f050009;
-        /**  Student Life North Mankato ATOM 
+        /**  Student Life North Mankato RSS 
          */
         public static final int Link4=0x7f05000a;
-        /**  Student Life Faribault ATOM 
+        /**  Student Life Faribault RSS 
          */
         public static final int Link5=0x7f05000b;
         /**  D2L Canceled Courses RSS 
@@ -65,7 +60,7 @@ public final class R {
         public static final int Link6=0x7f05000c;
         public static final int RssDate=0x7f05000e;
         public static final int RssDetail=0x7f05000f;
-        /**  Academic Calendar ATOM 
+        /**  Academic Calendar RSS 
          */
         public static final int RssTitle=0x7f05000d;
         public static final int app_name=0x7f050000;
