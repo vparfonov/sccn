5bcf5e6d5851d86e73f17d8411685cb8789c0d66
Tyler Holmgren <tylerholmgren@nerdev.co>
2014-12-07 17:32:11 -0600

    test

---
+++ gen/pw/tjh/android/SCCNotifications/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package pw.tjh.android.SCCNotifications;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
